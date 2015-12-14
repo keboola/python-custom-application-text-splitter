c871bec7caf7dbcfd8ef175baefa89f319a09d65
Odin <odinuv@gmail.com>
2015-12-14 19:24:36 +0100

    feat: distinguish between user and application errors

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import textSplitter
+import sys
+import traceback
 
-app = textSplitter.App()
-app.run()
+try:
+	app = textSplitter.App()
+	app.run()
+except ValueError as err:
+	print(err, file=sys.stderr)
+	sys.exit(1)
+except Exception as err:
+	print(err, file=sys.stderr)
+	traceback.print_exc(file=sys.stderr)
+	sys.exit(2)
