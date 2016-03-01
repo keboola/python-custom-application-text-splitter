298016f85a50877abe664595a9e203b1a77fd13d
Daniel Milde <daniel.milde@firma.seznam.cz>
2016-03-01 13:25:58 +0100

    PEP8 names
    * Changed module, function and variable names to conform to PEP8
    * Replaced line endings in bash to LF
    * Replaced TAB indention

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import textSplitter
+import text_splitter
 import sys
 import traceback
 
 try:
-	app = textSplitter.App()
-	app.run()
+    app = text_splitter.App()
+    app.run()
 except ValueError as err:
-	print(err, file=sys.stderr)
-	sys.exit(1)
+    print(err, file=sys.stderr)
+    sys.exit(1)
 except Exception as err:
-	print(err, file=sys.stderr)
-	traceback.print_exc(file=sys.stderr)
-	sys.exit(2)
+    print(err, file=sys.stderr)
+    traceback.print_exc(file=sys.stderr)
+    sys.exit(2)
--- test/test_app.py
+++ test/test_app.py
@@ -1,24 +1,25 @@
-import textSplitter
+import text_splitter
 import os
 import csv
 
-class TestTextSplitter:
-    def test_textSplitter(self, dataDir):
-        dataDir = os.getenv('KBC_DATA_DIR')        
-        app = textSplitter.App()
-        app.run()
-        
-        assert(os.path.isfile(dataDir + '/out/tables/result.csv'))
-        sampleData = []
-        with open(dataDir + '/sample-result.csv', 'rt') as sample:
-            csvReader = csv.DictReader(sample, delimiter = ',', quotechar = '"')
-            for row in csvReader:
-                sampleData.append(row)
-                
-        resultData = []
-        with open(dataDir + '/out/tables/result.csv', 'rt') as sample:
-            csvReader = csv.DictReader(sample, delimiter = ',', quotechar = '"')
-            for row in csvReader:
-                resultData.append(row)
 
-        assert(sampleData == resultData)
+class TestTextSplitter:
+    def test_text_splitter(self, data_dir):
+        data_dir = os.getenv('KBC_DATA_DIR')
+        app = text_splitter.App()
+        app.run()
+
+        assert(os.path.isfile(data_dir + '/out/tables/result.csv'))
+        sample_data = []
+        with open(data_dir + '/sample-result.csv', 'rt') as sample:
+            csv_reader = csv.DictReader(sample, delimiter=',', quotechar='"')
+            for row in csv_reader:
+                sample_data.append(row)
+
+        result_data = []
+        with open(data_dir + '/out/tables/result.csv', 'rt') as sample:
+            csv_reader = csv.DictReader(sample, delimiter=',', quotechar='"')
+            for row in csv_reader:
+                result_data.append(row)
+
+        assert(sample_data == result_data)
